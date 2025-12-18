cda7469318b42f18e886785ef1f2b99762a837cd
hakunijun <trimini123456@gmail.com>
2025-12-18 21:56:18 +0700

    feat:lich su mua hang & theo doi don hang & xac nhan hang

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -92,4 +92,65 @@ class OrderController extends Controller
             return redirect()->route('order.history')->with('success','Đã hủy đơn hàng !');
         return redirect()->route('order.history')->with('error','Đã có lỗi xảy ra, vui lòng thử lại sau !');
     }
+    
+    function myHistory(){
+        if(session('user_id')){
+            $data = [];
+            // danh sách order đã duyệt
+            $orders = DB::table('donhang')
+                        ->join('khachhang','khachhang.id_khach_hang','donhang.id_khach_hang')
+                        ->select('id_don_hang','fullname','tong_tien','loai_thanh_toan','ngay_giao','ngay_dat','trang_thai_don_hang')
+                        ->whereIn('trang_thai_don_hang',[7,8])
+                        ->where('khachhang.id_khach_hang','=',session('user_id'))
+                        ->orderBy('ngay_dat','desc')
+                        ->get();
+            $data["order"] = $orders;
+            return view('history')->with('data',$data);
+        }
+        return  redirect()->route('order.index')->with('error','Không thể xem lịch sử, vui lòng thử lại sau !');
+    }
+    
+    function detailOneOfHistory($idDonHang){
+        $data = [];
+        $donHang = DB::table('donhang')
+                    ->join('khachhang','khachhang.id_khach_hang','donhang.id_khach_hang')
+                    ->select('id_don_hang','fullname','tong_tien','loai_thanh_toan','ngay_dat','dia_chi','sdt')
+                    ->where('donhang.id_don_hang','=',$idDonHang)
+                    ->get();
+        if(count($donHang) < 1)
+            return redirect()->route('order.index')->with('error','Đã có lỗi xảy ra, vui lòng thủ lại sau !');
+
+        $chitiet =  DB::table('donhang')
+                    ->join('chitietdonhang','donhang.id_don_hang','chitietdonhang.id_don_hang')
+                    ->join('chitietthietbi','chitietthietbi.id_chi_tiet_thiet_bi','chitietdonhang.id_chi_tiet_thiet_bi')
+                    ->select('ten','gia_ban','src_anh','so_luong','chitietdonhang.tong_tien')
+                    ->where('chitietdonhang.id_don_hang','=',$idDonHang)
+                    ->get();
+        
+        $data["donHang"] = $donHang;
+        $data["chitiet"] = $chitiet;
+        return view('history')->with('data',$data);
+    }
+
+    function huyDon(Request $request){
+        $data = DB::table('donhang')
+                    ->where('id_don_hang','=',$request->input('idDonHang'))
+                    ->update([
+                        'trang_thai_don_hang' => 7
+                    ]);
+        if($data > 0)
+            return redirect()->route('order.history')->with('success','Đã hủy đơn hàng !');
+        return redirect()->route('order.history')->with('error','Đã có lỗi xảy ra, vui lòng thử lại sau !');
+    }
+
+    function daNhanDuocHang(Request $request){
+        $data = DB::table('donhang')
+        ->where('id_don_hang','=',$request->input('idDonHang'))
+        ->update([
+            'trang_thai_don_hang' => 8
+        ]);
+        if($data > 0)
+            return redirect()->route('order.history')->with('success','Cảm ơn đã phản hồi đơn hàng !');
+        return redirect()->route('order.history')->with('error','Đã có lỗi xảy ra, vui lòng thử lại sau !');
+    }
 }
