1eb5a64484d4dbba3e542c7395aa08ed586eac9d
Nguyen Minh Tam <nminhtam425@gmail.com>
2025-12-18 20:52:53 +0700

    feat: UI admin va chuc nang login admin

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
+use App\Http\Controllers\AdminController;
 
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Trang chủ
+Route::get('/', [ProductController::class,'products'])->name('home');
 
-Route::get('/', function () {
-    return view('home');
+//admin
+Route::get('/trang-chu/login',[AdminController::class, 'login'])->name('admin.login');
+Route::post('/trang-chu/login',[AdminController::class, 'handleLogin'])->name('admin.handleLogin');
+Route::middleware(['admin.check'])->group(function () {
+    Route::prefix('trang-chu')->group(function () {
+        Route::get('/',[AdminController::class,'index'])->name('admin.home');
+    });
 });
