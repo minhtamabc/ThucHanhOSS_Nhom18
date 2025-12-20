c4a7bcdd40287992d7c254c866f5533e999c402f
Nguyen Minh Tam <nminhtam425@gamil.com>
2025-12-20 07:54:07 +0700

    feat: tich hop dich vu thanh toan vnpaay

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -56,9 +56,81 @@ class OrderController extends Controller
                 if($data > 0) 
                   return redirect()->route('order.index')->with('success','Đã đặt hàng thành công !');
             }
+            //vnpay
+            else if($cod == "2"){
+                $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+                $vnp_Returnurl = "http://127.0.0.1:8000/vnpay-return/";
+                $vnp_TmnCode = "5XVFV4NZ";
+                $vnp_HashSecret = "13D02GO4YMBASUAPINGOQFEV67EZ4APH"; 
+                    
+                $vnp_TxnRef = $idDonHang; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này 
+                $vnp_OrderInfo = 'Thanh toán dịch vụ';
+                $vnp_OrderType = 'billpayment';
+                $vnp_Amount = $total * 100;
+                $vnp_Locale = 'vn';
+                $vnp_BankCode = 'VNBANK';
+                $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+                //Add Params of 2.0.1 Version
+                $vnp_ExpireDate = date("YmdHis",strtotime(date("YmdHis")."+10 minute"));
+            
+                $inputData = array(
+                    "vnp_Version" => "2.1.0",
+                    "vnp_TmnCode" => $vnp_TmnCode,
+                    "vnp_Amount" => $vnp_Amount,
+                    "vnp_Command" => "pay",
+                    "vnp_CreateDate" => date('YmdHis'),
+                    "vnp_CurrCode" => "VND",
+                    "vnp_IpAddr" => $vnp_IpAddr,
+                    "vnp_Locale" => $vnp_Locale,
+                    "vnp_OrderInfo" => $vnp_OrderInfo,
+                    "vnp_OrderType" => $vnp_OrderType,
+                    "vnp_ReturnUrl" => $vnp_Returnurl,
+                    "vnp_TxnRef" => $vnp_TxnRef,
+                    "vnp_ExpireDate"=>$vnp_ExpireDate
+                );
+                
+                if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+                    $inputData['vnp_BankCode'] = $vnp_BankCode;
+                }
+                if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
+                    $inputData['vnp_Bill_State'] = $vnp_Bill_State;
+                }
+                
+                //var_dump($inputData);
+                ksort($inputData);
+                $query = "";
+                $i = 0;
+                $hashdata = "";
+                foreach ($inputData as $key => $value) {
+                    if ($i == 1) {
+                        $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+                    } else {
+                        $hashdata .= urlencode($key) . "=" . urlencode($value);
+                        $i = 1;
+                    }
+                    $query .= urlencode($key) . "=" . urlencode($value) . '&';
+                }
+                
+                $vnp_Url = $vnp_Url . "?" . $query;
+                if (isset($vnp_HashSecret)) {
+                    $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
+                    $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+                }
+
+                $data = DB::table('donhang')
+                            ->where('id_don_hang','=',$idDonHang)
+                            ->update([
+                                'tong_tien' => ($total/1000),
+                                'ngay_dat' => date('Y-m-d H:i:s'),
+                                'loai_thanh_toan' => 2
+                            ]);
+
+                return redirect($vnp_Url);
+            }
         }
         return redirect()->route('order.index')->with('error','Đã có lỗi xảy ra, vui lòng thử lại sau !');
     }
+
     
     function detailOneOfHistory($idDonHang){
         $data = [];
