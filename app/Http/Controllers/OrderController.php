0ab34982ec3df445843a05951215f19ab65bc63a
unknown <phuc202103@gmail.com>
2025-12-23 18:29:48 +0700

    feat:chi Tiet Lich Su Cua Mot Don Hang

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -24,7 +24,28 @@ class OrderController extends Controller
         return view('myOrder')->with('data',$data);
        
     }
+ function detailOneOfHistory($idDonHang){
+        $data = [];
+        $donHang = DB::table('donhang')
+                    ->join('khachhang','khachhang.id_khach_hang','donhang.id_khach_hang')
+                    ->select('id_don_hang','fullname','tong_tien','loai_thanh_toan','ngay_dat','dia_chi','sdt')
+                    ->where('donhang.id_don_hang','=',$idDonHang)
+                    ->get();
+        // bug xem chi tiet lich su
+        // if(count($donHang) < 1)
+        //     return redirect()->route('order.index')->with('error','Đã có lỗi xảy ra, vui lòng thủ lại sau !');
 
+        $chitiet =  DB::table('donhang')
+                    ->join('chitietdonhang','donhang.id_don_hang','chitietdonhang.id_don_hang')
+                    ->join('chitietthietbi','chitietthietbi.id_chi_tiet_thiet_bi','chitietdonhang.id_chi_tiet_thiet_bi')
+                    ->select('ten','gia_ban','src_anh','so_luong','chitietdonhang.tong_tien')
+                    ->where('chitietdonhang.id_don_hang','=',$idDonHang)
+                    ->get();
+        
+        $data["donHang"] = $donHang;
+        $data["chitiet"] = $chitiet;
+        return view('history')->with('data',$data);
+    }
     
 
     function myHistory(){
