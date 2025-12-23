6ddf94e6b6fc478d7fecf10ede86f665855e764c
Nguyen Minh Tam <nminhtam425@gmail.com>
2025-12-23 15:32:54 +0700

    init middleware admin and user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin.check' => \App\Http\Middleware\AdminCheck::class,
+        'check.login' => \App\Http\Middleware\AdminCheck::class,
     ];
 }
