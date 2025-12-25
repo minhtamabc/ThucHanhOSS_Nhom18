5b6b580430f93575981579e9cf581f41f3dd2560
hakunijun <trimini123456@gmail.com>
2025-12-25 21:03:21 +0700

    feat:Them san pham vao gio hang

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -49,5 +49,104 @@ class CartController extends Controller
 
         return view('cart', compact('cartItems', 'total'));
     }
+   public function add(Request $request)
+    {
+        if(isset($_POST["btnThemVaoGio"])){
+            $idProduct = $_POST["btnThemVaoGio"];
+            //kiểm tra xem có đặt hàng chưa
+            $donhang = DB::table('donhang')
+                        ->select('id_don_hang')
+                        ->where('id_khach_hang','=',session('user_id'))
+                        ->where('trang_thai_don_hang',6)
+                        ->get();
 
+            $giaSP = DB::table('chitietthietbi')
+                    ->select('gia_ban')
+                    ->where('id_chi_tiet_thiet_bi','=',$idProduct)
+                    ->get();
+
+            if($giaSP[0]->gia_ban !== '')        
+                $giaSP = (int)($giaSP[0]->gia_ban);
+            else
+                return redirect()->route('cart.index')->with('error', 'Đã có lỗi xảy ra, vui lòng thử lại sau !');
+
+            //chưa đặt
+            $soLuong = 1;
+            if(!count($donhang)){
+                $idDonHang = 'DH'.Str::random(8);
+
+                $data = DB::table('donhang')->insert( [
+                    'id_don_hang' => $idDonHang,
+                    'id_khach_hang' => session('user_id'),
+                    'trang_thai_don_hang' => 6
+                ]);
+
+                DB::table('chitietdonhang')->insert([
+                    'id_don_hang' => $idDonHang,
+                    'id_chi_tiet_thiet_bi' => $idProduct,
+                    'so_luong' => $soLuong,
+                    'tong_tien' => $giaSP*$soLuong
+                ]);
+            }
+            else{
+                //đã đặt - check sản phẩm
+                $idDH = DB::table('chitietdonhang')
+                            ->select('id_don_hang','so_luong')
+                            ->where('id_chi_tiet_thiet_bi','=',$idProduct)
+                            ->where('id_don_hang','=',$donhang[0]->id_don_hang)
+                            ->get();
+                // chưa tồn tại sản phẩm
+                if(!count($idDH)){
+                    DB::table('chitietdonhang')->insert([
+                        'id_don_hang' => $donhang[0]->id_don_hang,
+                        'id_chi_tiet_thiet_bi' => $idProduct,
+                        'so_luong' => 1,
+                        'tong_tien' => $giaSP*$soLuong
+                    ]);
+                }
+                else{
+                    DB::table('chitietdonhang')
+                        ->where('id_don_hang','=',$idDH[0]->id_don_hang)
+                        ->update([
+                            'so_luong' => (int)$idDH[0]->so_luong + 1,
+                            'tong_tien' => (int)$giaSP*($idDH[0]->so_luong + 1)
+                        ]);
+                }
+            }
+            return redirect()->route('cart.index')->with('sucess', 'Cập nhật vào giỏ hảng thành công !');
+        }
+        return redirect()->route('cart.index')->with('error', 'Đã có lỗi xảy ra, vui lòng thử lại sau !');
+    }
+
+    // Cập nhật số lượng
+    public function update(Request $request, $idProduct,$idDonHang)
+    {
+        if(isset($_POST["update-quantity"])){
+            $dataPrice = DB::table('chitietthietbi')
+                            ->select('gia_ban')
+                            ->where('id_chi_tiet_thiet_bi','=',$idProduct)
+                            ->get();
+
+            $giaSP = (int)$dataPrice[0]->gia_ban;
+            $soLuong = (int) $_POST["quantity"];
+            DB::table('chitietdonhang')
+                ->where('id_don_hang','=',$idDonHang)
+                ->where('id_chi_tiet_thiet_bi','=',$idProduct)
+                ->update([
+                    'so_luong' => $soLuong,
+                    'tong_tien' => $soLuong*$giaSP
+                ]);
+            return redirect()->route('cart.index')->with('success', 'Đã cập nhật số lượng thành công!');
+        }
+        return redirect()->route('cart.index')->with('error', 'Đã có lỗi xảy ra, vui lòng thử lại sau !');
+    }
+   public function countCart($idKhachHang){
+        $data = DB::table('donhang')
+                    ->join('chitietdonhang','donhang.id_don_hang','chitietdonhang.id_don_hang')
+                    ->select('chitietdonhang.id_don_hang')
+                    ->where('donhang.id_khach_hang','=',$idKhachHang)
+                    ->where('trang_thai_don_hang','=',6)
+                    ->get();
+        return count($data);
+    }
 }
